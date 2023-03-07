6f54da64b8547f7b902baf6b80c8eb30f00493a2
Adrien Olivon <adrien.olivon.dev@gmail.com>
2023-03-07 19:01:59 +0100

    Add example with ready_when

---
+++ tearup_examples/tests/using_when_ready.rs
@@ -0,0 +1,57 @@
+use async_trait::async_trait;
+use std::time::Duration;
+use tearup::{
+    async_ready_when, ready_when, tearup_test, AsyncContext, Context, FutureExt, ReadyFn,
+};
+
+#[tearup_test(AsyncReadyWhenContext)]
+async fn setup_barely_timeout_with_ready_when() {}
+
+struct AsyncReadyWhenContext;
+
+#[async_trait]
+impl AsyncContext<'_> for AsyncReadyWhenContext {
+    async fn setup(ready: ReadyFn) -> Self {
+        launch_server();
+
+        async_ready_when(
+            ready,
+            || async move { ping_server().await.is_ok() }.boxed(),
+            Duration::from_millis(100),
+        )
+        .await;
+
+        Self {}
+    }
+
+    async fn teardown(&mut self) {}
+}
+
+fn launch_server() {}
+
+async fn ping_server() -> Result<(), ()> {
+    Ok(())
+}
+
+struct SyncReadyWhenContext;
+
+#[async_trait]
+impl Context for SyncReadyWhenContext {
+    fn setup(ready: ReadyFn) -> Self {
+        launch_server();
+
+        ready_when(
+            ready,
+            Box::new(|| sync_ping_server().is_ok()),
+            Duration::from_millis(100),
+        );
+
+        Self {}
+    }
+
+    fn teardown(&mut self) {}
+}
+
+fn sync_ping_server() -> Result<(), ()> {
+    Ok(())
+}
