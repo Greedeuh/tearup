c9336219745db27853a4dcd3db6d278a4b8dd2cb
Adrien Olivon <adrien.olivon.dev@gmail.com>
2023-03-15 22:45:27 +0100

    Move FromContext to context module

--- tearup/src/contexts/mod.rs
+++ tearup/src/contexts/mod.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "async")]
+use async_trait::async_trait;
+#[cfg(feature = "async")]
 use futures::future::BoxFuture;
 use std::any::Any;
 #[cfg(feature = "async")]
@@ -12,6 +14,18 @@ mod simple;
 pub use simple::*;
 mod sequential_combinator;
 pub use sequential_combinator::*;
+
+/// Trait to implement if you need to access a setup value in you test.
+pub trait FromContext<C: WaitingContext> {
+    fn from_context(context: &C) -> Self;
+}
+
+/// Trait to implement if you need to access a setup value in you test.
+#[cfg(feature = "async")]
+#[async_trait]
+pub trait FromAsyncContext<'a, C: AsyncWaitingContext<'a>> {
+    async fn from_context(context: &C) -> Self;
+}
 
 pub(crate) fn launch_test<TestFn>(test: TestFn) -> Result<(), Box<dyn Any + Send>>
 where
--- tearup/src/lib.rs
+++ tearup/src/lib.rs
@@ -6,15 +6,3 @@ mod contexts;
 pub use contexts::*;
 mod ready;
 pub use ready::*;
-
-/// Trait to implement if you need to access a setup value in you test.
-pub trait FromContext<C: WaitingContext> {
-    fn from_context(context: &C) -> Self;
-}
-
-/// Trait to implement if you need to access a setup value in you test.
-#[cfg(feature = "async")]
-#[async_trait]
-pub trait FromAsyncContext<'a, C: AsyncWaitingContext<'a>> {
-    async fn from_context(context: &C) -> Self;
-}
