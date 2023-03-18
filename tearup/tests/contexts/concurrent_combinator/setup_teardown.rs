8873fe7db5125eb661bf2f601ee72d069d1206ff
Adrien Olivon <adrien.olivon.dev@gmail.com>
2023-03-18 12:40:24 +0100

    Improve checkpoints tests

--- tearup/tests/contexts/concurrent_combinator/setup_teardown.rs
+++ tearup/tests/contexts/concurrent_combinator/setup_teardown.rs
@@ -1,20 +1,31 @@
 use lazy_static::lazy_static;
-use std::{sync::Mutex, time::Duration};
+use std::{
+    sync::Mutex,
+    time::{Duration, SystemTime},
+};
 use tearup::{tearup, ConcurrentContextCombinator, SimpleContext};
 
 use crate::helper::assert_around_100ms;
 
 lazy_static! {
-    static ref FIRST_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
-    static ref SECOND_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
+    static ref FIRST_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref FIRST_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref SECOND_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref SECOND_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
 }
 
 #[test]
 fn it_pass_through_setup_then_teardown_of_both_contexts_concurrently() {
     assert_around_100ms(concurrent);
 
-    assert!(FIRST_CHECKPOINT.lock().unwrap().unwrap());
-    assert!(SECOND_CHECKPOINT.lock().unwrap().unwrap());
+    assert!(
+        FIRST_SETUP_CHECKPOINT.lock().unwrap().unwrap()
+            < FIRST_TEARDOWN_CHECKPOINT.lock().unwrap().unwrap()
+    );
+    assert!(
+        SECOND_SETUP_CHECKPOINT.lock().unwrap().unwrap()
+            < SECOND_TEARDOWN_CHECKPOINT.lock().unwrap().unwrap()
+    );
 }
 
 type B = ConcurrentContextCombinator<FirstContext, SecondContext>;
@@ -24,34 +35,40 @@ fn concurrent() {}
 pub struct FirstContext;
 impl SimpleContext for FirstContext {
     fn setup() -> Self {
-        let mut checkpoint = FIRST_CHECKPOINT.lock().unwrap();
-        *checkpoint = Some(true);
+        let mut checkpoint = FIRST_SETUP_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
 
         std::thread::sleep(Duration::from_millis(100));
 
         Self {}
     }
 
-    fn teardown(&mut self) {}
+    fn teardown(&mut self) {
+        let mut checkpoint = FIRST_TEARDOWN_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
+    }
 }
 
 pub struct SecondContext;
 impl SimpleContext for SecondContext {
     fn setup() -> Self {
-        let mut checkpoint = SECOND_CHECKPOINT.lock().unwrap();
-        *checkpoint = Some(true);
+        let mut checkpoint = SECOND_SETUP_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
 
         std::thread::sleep(Duration::from_millis(100));
 
         Self {}
     }
 
-    fn teardown(&mut self) {}
+    fn teardown(&mut self) {
+        let mut checkpoint = SECOND_TEARDOWN_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
+    }
 }
 
 mod asyncc {
     use lazy_static::lazy_static;
-    use std::time::Duration;
+    use std::time::{Duration, SystemTime};
     use tearup::{
         async_trait, tearup, AsyncConcurrentContextCombinator, AsyncSimpleContext, FutureExt,
     };
@@ -60,16 +77,24 @@ mod asyncc {
     use crate::helper::async_assert_around_100ms;
 
     lazy_static! {
-        static ref FIRST_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
-        static ref SECOND_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
+        static ref FIRST_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref FIRST_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref SECOND_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref SECOND_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
     }
 
     #[tokio::test]
     async fn it_pass_through_setup_then_teardown_of_both_contexts_concurrently() {
         async_assert_around_100ms(move || { async move { concurrent().await } }.boxed()).await;
 
-        assert!(FIRST_CHECKPOINT.lock().await.unwrap());
-        assert!(SECOND_CHECKPOINT.lock().await.unwrap());
+        assert!(
+            FIRST_SETUP_CHECKPOINT.lock().await.unwrap()
+                < FIRST_TEARDOWN_CHECKPOINT.lock().await.unwrap()
+        );
+        assert!(
+            SECOND_SETUP_CHECKPOINT.lock().await.unwrap()
+                < SECOND_TEARDOWN_CHECKPOINT.lock().await.unwrap()
+        );
     }
 
     type B = AsyncConcurrentContextCombinator<FirstContext, SecondContext>;
@@ -80,29 +105,35 @@ mod asyncc {
     #[async_trait]
     impl AsyncSimpleContext<'_> for FirstContext {
         async fn setup() -> Self {
-            let mut checkpoint = FIRST_CHECKPOINT.lock().await;
-            *checkpoint = Some(true);
+            let mut checkpoint = FIRST_SETUP_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
 
             sleep(Duration::from_millis(100)).await;
 
             Self {}
         }
 
-        async fn teardown(&mut self) {}
+        async fn teardown(&mut self) {
+            let mut checkpoint = FIRST_TEARDOWN_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
+        }
     }
 
     pub struct SecondContext;
     #[async_trait]
     impl AsyncSimpleContext<'_> for SecondContext {
         async fn setup() -> Self {
-            let mut checkpoint = SECOND_CHECKPOINT.lock().await;
-            *checkpoint = Some(true);
+            let mut checkpoint = SECOND_SETUP_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
 
             sleep(Duration::from_millis(100)).await;
 
             Self {}
         }
 
-        async fn teardown(&mut self) {}
+        async fn teardown(&mut self) {
+            let mut checkpoint = SECOND_TEARDOWN_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
+        }
     }
 }
--- tearup/tests/contexts/sequential_combinator/setup_teardown.rs
+++ tearup/tests/contexts/sequential_combinator/setup_teardown.rs
@@ -1,20 +1,31 @@
 use lazy_static::lazy_static;
-use std::{sync::Mutex, time::Duration};
+use std::{
+    sync::Mutex,
+    time::{Duration, SystemTime},
+};
 use tearup::{tearup, SequentialContextCombinator, SimpleContext};
 
 use crate::helper::assert_around_100ms;
 
 lazy_static! {
-    static ref FIRST_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
-    static ref SECOND_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
+    static ref FIRST_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref FIRST_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref SECOND_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+    static ref SECOND_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
 }
 
 #[test]
 fn it_pass_through_setup_then_teardown_of_both_contexts_one_after_the_other() {
     assert_around_100ms(sequential);
 
-    assert!(FIRST_CHECKPOINT.lock().unwrap().unwrap());
-    assert!(SECOND_CHECKPOINT.lock().unwrap().unwrap());
+    assert!(
+        FIRST_SETUP_CHECKPOINT.lock().unwrap().unwrap()
+            < SECOND_SETUP_CHECKPOINT.lock().unwrap().unwrap()
+    );
+    assert!(
+        FIRST_TEARDOWN_CHECKPOINT.lock().unwrap().unwrap()
+            < SECOND_TEARDOWN_CHECKPOINT.lock().unwrap().unwrap()
+    );
 }
 
 type B = SequentialContextCombinator<FirstContext, SecondContext>;
@@ -24,34 +35,40 @@ fn sequential() {}
 pub struct FirstContext;
 impl SimpleContext for FirstContext {
     fn setup() -> Self {
-        let mut checkpoint = FIRST_CHECKPOINT.lock().unwrap();
-        *checkpoint = Some(true);
+        let mut checkpoint = FIRST_SETUP_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
 
         std::thread::sleep(Duration::from_millis(50));
 
         Self {}
     }
 
-    fn teardown(&mut self) {}
+    fn teardown(&mut self) {
+        let mut checkpoint = FIRST_TEARDOWN_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
+    }
 }
 
 pub struct SecondContext;
 impl SimpleContext for SecondContext {
     fn setup() -> Self {
-        let mut checkpoint = SECOND_CHECKPOINT.lock().unwrap();
-        *checkpoint = Some(true);
+        let mut checkpoint = SECOND_SETUP_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
 
         std::thread::sleep(Duration::from_millis(50));
 
         Self {}
     }
 
-    fn teardown(&mut self) {}
+    fn teardown(&mut self) {
+        let mut checkpoint = SECOND_TEARDOWN_CHECKPOINT.lock().unwrap();
+        *checkpoint = Some(SystemTime::now());
+    }
 }
 
 mod asyncc {
     use lazy_static::lazy_static;
-    use std::time::Duration;
+    use std::time::{Duration, SystemTime};
     use tearup::{
         async_trait, tearup, AsyncSequentialContextCombinator, AsyncSimpleContext, FutureExt,
     };
@@ -60,16 +77,24 @@ mod asyncc {
     use crate::helper::async_assert_around_100ms;
 
     lazy_static! {
-        static ref FIRST_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
-        static ref SECOND_CHECKPOINT: Mutex<Option<bool>> = Some(false).into();
+        static ref FIRST_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref FIRST_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref SECOND_SETUP_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
+        static ref SECOND_TEARDOWN_CHECKPOINT: Mutex<Option<SystemTime>> = None.into();
     }
 
     #[tokio::test]
     async fn it_pass_through_setup_then_teardown_of_both_contexts_one_after_the_other() {
         async_assert_around_100ms(move || { async move { sequential().await } }.boxed()).await;
 
-        assert!(FIRST_CHECKPOINT.lock().await.unwrap());
-        assert!(SECOND_CHECKPOINT.lock().await.unwrap());
+        assert!(
+            FIRST_SETUP_CHECKPOINT.lock().await.unwrap()
+                < SECOND_SETUP_CHECKPOINT.lock().await.unwrap()
+        );
+        assert!(
+            FIRST_TEARDOWN_CHECKPOINT.lock().await.unwrap()
+                < SECOND_TEARDOWN_CHECKPOINT.lock().await.unwrap()
+        );
     }
 
     type B = AsyncSequentialContextCombinator<FirstContext, SecondContext>;
@@ -80,29 +105,35 @@ mod asyncc {
     #[async_trait]
     impl AsyncSimpleContext<'_> for FirstContext {
         async fn setup() -> Self {
-            let mut checkpoint = FIRST_CHECKPOINT.lock().await;
-            *checkpoint = Some(true);
+            let mut checkpoint = FIRST_SETUP_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
 
             sleep(Duration::from_millis(50)).await;
 
             Self {}
         }
 
-        async fn teardown(&mut self) {}
+        async fn teardown(&mut self) {
+            let mut checkpoint = FIRST_TEARDOWN_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
+        }
     }
 
     pub struct SecondContext;
     #[async_trait]
     impl AsyncSimpleContext<'_> for SecondContext {
         async fn setup() -> Self {
-            let mut checkpoint = SECOND_CHECKPOINT.lock().await;
-            *checkpoint = Some(true);
+            let mut checkpoint = SECOND_SETUP_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
 
             sleep(Duration::from_millis(50)).await;
 
             Self {}
         }
 
-        async fn teardown(&mut self) {}
+        async fn teardown(&mut self) {
+            let mut checkpoint = SECOND_TEARDOWN_CHECKPOINT.lock().await;
+            *checkpoint = Some(SystemTime::now());
+        }
     }
 }
