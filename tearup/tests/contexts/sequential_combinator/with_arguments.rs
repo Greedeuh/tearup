fa611a0a9bc27ecaaa41aa332811928ffcc303cd
Adrien Olivon <adrien.olivon.dev@gmail.com>
2023-04-02 19:05:52 +0200

    WIP

--- tearup/tests/contexts/sequential_combinator/with_arguments.rs
+++ tearup/tests/contexts/sequential_combinator/with_arguments.rs
@@ -23,44 +23,33 @@ fn it_uses_some_variables(mut a: FirstProof, b: SecondProof, c: ThirdProof, d: F
 
 #[cfg(feature = "async")]
 mod asyncc {
-    use async_trait::async_trait;
-    use lazy_static::lazy_static;
-    use std::time::{Duration, SystemTime};
-    use tearup::{tearup, AsyncSharedContext, AsyncSimpleContext};
-    use tokio::time::sleep;
+    use tearup::{tearup, AsyncSequentialContextCombinator};
 
-    use crate::helper::{assert_async_order, AsyncCheckpoint};
-
-    lazy_static! {
-        static ref SETUP_CHECKPOINT: AsyncCheckpoint = None.into();
-        static ref TEARDOWN_CHECKPOINT: AsyncCheckpoint = None.into();
-    }
+    use crate::helper::{
+        AsyncFirstFullContext, AsyncSecondFullContext, FirstProof, FourthProof, SecondProof,
+        ThirdProof,
+    };
 
     #[tokio::test]
-    async fn it_pass_through_setup_then_teardown() {
-        teardown_panic().await;
-
-        assert_async_order(&SETUP_CHECKPOINT, &TEARDOWN_CHECKPOINT).await;
+    async fn it_gives_access_to_variables() {
+        it_uses_some_variables().await;
     }
 
-    struct NiceContext;
-    #[async_trait]
-    impl AsyncSimpleContext<'_> for NiceContext {
-        async fn setup(_shared_context: AsyncSharedContext) -> Self {
-            let mut checkpoint = SETUP_CHECKPOINT.lock().await;
-            *checkpoint = Some(SystemTime::now());
+    type Combination =
+        AsyncSequentialContextCombinator<AsyncFirstFullContext, AsyncSecondFullContext>;
 
-            sleep(Duration::from_millis(10)).await;
-
-            Self {}
-        }
-
-        async fn teardown(mut self, _shared_context: AsyncSharedContext) {
-            let mut checkpoint = TEARDOWN_CHECKPOINT.lock().await;
-            *checkpoint = Some(SystemTime::now());
-        }
+    #[tearup(Combination)]
+    async fn it_uses_some_variables(
+        mut a: FirstProof,
+        b: SecondProof,
+        c: ThirdProof,
+        d: FourthProof,
+    ) {
+        assert_eq!(a.0, "first_proof");
+        a.0 = "mutable".to_owned();
+        assert_eq!(a.0, "mutable");
+        assert_eq!(b.0, "second_proof");
+        assert_eq!(c.0, "ref_to_first_proof");
+        assert_eq!(d.0, "another_ref_to_second_proof");
     }
-
-    #[tearup(NiceContext)]
-    async fn teardown_panic() {}
 }
--- tearup/tests/helper/mod.rs
+++ tearup/tests/helper/mod.rs
@@ -41,7 +41,42 @@ impl SimpleContext for SecondFullContext {
 
 #[cfg(feature = "async")]
 pub mod asyncc {
-    use async_trait::async_trait;
-    use tearup::{ReadyChecksConfig, ReadyFn};
-    use tokio::{spawn, time::sleep};
+    use tearup::{async_trait, AsyncSharedContext, AsyncSimpleContext};
+
+    use crate::helper::{FirstProof, FourthProof, SecondProof, ThirdProof};
+
+    pub struct AsyncFirstFullContext;
+    #[async_trait]
+    impl AsyncSimpleContext<'_> for AsyncFirstFullContext {
+        async fn setup(shared_context: AsyncSharedContext) -> Self {
+            shared_context
+                .register(FirstProof("first_proof".to_owned()))
+                .await;
+            shared_context
+                .register(SecondProof("second_proof".to_owned()))
+                .await;
+            Self {}
+        }
+
+        async fn teardown(self, _shared_context: AsyncSharedContext) {}
+    }
+
+    pub struct AsyncSecondFullContext;
+    #[async_trait]
+    impl AsyncSimpleContext<'_> for AsyncSecondFullContext {
+        async fn setup(mut shared_context: AsyncSharedContext) -> Self {
+            let first = shared_context.get::<FirstProof>().await.unwrap().0;
+            shared_context
+                .register(ThirdProof(format!("ref_to_{}", first)))
+                .await;
+
+            let second = shared_context.get::<SecondProof>().await.unwrap().0;
+            shared_context
+                .register(FourthProof(format!("another_ref_to_{}", second)))
+                .await;
+            Self {}
+        }
+
+        async fn teardown(self, _shared_context: AsyncSharedContext) {}
+    }
 }
