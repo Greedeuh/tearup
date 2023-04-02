9159f9c1c631c33486ed5a747b01b218beba8a0b
Adrien Olivon <adrien.olivon.dev@gmail.com>
2023-04-02 19:14:19 +0200

    Trash useless helpers

--- tearup/src/ready.rs
+++ tearup/src/ready.rs
@@ -8,21 +8,6 @@ pub type ReadyFlag = Arc<Mutex<bool>>;
 pub type ReadyFn = Box<dyn Fn() + Send + Sync>;
 pub type PredicateFn = Box<dyn Fn() -> bool + Send + Sync>;
 
-pub fn ready_state() -> (ReadyFlag, ReadyFn) {
-    let ready_flag = std::sync::Arc::new(std::sync::Mutex::new(false));
-
-    let ready = {
-        let ready_flag = ready_flag.clone();
-
-        Box::new(move || {
-            let mut ready = ready_flag.lock().unwrap();
-            *ready = true;
-        })
-    };
-
-    (ready_flag, ready)
-}
-
 #[derive(PartialEq, Debug)]
 pub struct ReadyChecksConfig {
     pub duration: Duration,
@@ -59,66 +44,6 @@ pub fn ready_when(ready: ReadyFn, predicate: PredicateFn, waiting_duration: Dura
         sleep(waiting_duration)
     }
     ready()
-}
-
-pub fn split(both_ready: ReadyFn) -> (ReadyFn, ReadyFn) {
-    let both_ready = Arc::new(both_ready);
-
-    let ready_flag1 = Arc::new(std::sync::Mutex::new(false));
-    let ready_flag2 = Arc::new(std::sync::Mutex::new(false));
-
-    let ready1 = {
-        let ready_flag1 = ready_flag1.clone();
-        let ready_flag2 = ready_flag2.clone();
-        let both_ready = both_ready.clone();
-
-        Box::new(move || {
-            let mut ready1 = ready_flag1.lock().unwrap();
-            let ready2 = ready_flag2.lock().unwrap();
-            *ready1 = true;
-            if *ready2 {
-                both_ready();
-            }
-        })
-    };
-
-    let ready2 = Box::new(move || {
-        let ready1 = ready_flag1.lock().unwrap();
-        let mut ready2 = ready_flag2.lock().unwrap();
-        *ready2 = true;
-        if *ready1 {
-            both_ready();
-        }
-    });
-    (ready1, ready2)
-}
-
-pub type SplitedReadyFn = Box<dyn Fn(u16) + Send + Sync>;
-
-pub fn n_times(all_ready: ReadyFn, n: u16) -> SplitedReadyFn {
-    let all_ready = Arc::new(all_ready);
-    let ready_flags = Arc::new(std::sync::Mutex::new(vec![false; n.into()]));
-
-    Box::new(move |who_index| {
-        let mut ready_flags = ready_flags.lock().unwrap();
-        ready_flags[who_index as usize] = true;
-
-        if ready_flags.iter().all(|x| *x) {
-            all_ready()
-        }
-    })
-}
-
-pub fn get_longest(config1: ReadyChecksConfig, config2: ReadyChecksConfig) -> ReadyChecksConfig {
-    let duration1 = config1.duration * config1.maximum.try_into().unwrap();
-
-    let duration2 = config1.duration * config2.maximum.try_into().unwrap();
-
-    if duration1 > duration2 {
-        config1
-    } else {
-        config2
-    }
 }
 
 #[cfg(feature = "async")]
